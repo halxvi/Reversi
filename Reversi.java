403e5cb7b6a2333b1cf95b6f04cf2a62a045db70
Kakeru Tsukayama <halxvi@gmail.com>
2020-01-16 18:27:15 +0900

    first commit

---
+++ Reversi.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+
+public class Reversi {
+    private static int turn = 0;
+    private static int field[][] = new int[9][9];
+
+    static void init() {
+        for (int i = 0; i < 9; i++) {
+            for (int n = 0; n < 9; n++) {
+                field[i][n] = 0;
+            }
+        }
+        field[4][4] = 10;
+        field[5][5] = 10;
+        field[4][5] = 9;
+        field[5][4] = 9;
+        for (int i = 1; i < 9; i++) {
+            field[0][i] = i;
+            field[i][0] = i;
+        }
+
+    }
+
+    static void showTable(int array[][]) {
+        for (int m = 0; m < 9; m++) {
+            for (int t = 0; t < 9; t++) {
+                if (array[m][t] == 0) {
+                    System.out.print("   ");
+                } else if (array[m][t] == 9) {
+                    System.out.print(" B ");
+                } else if (array[m][t] == 10) {
+                    System.out.print(" W ");
+                } else {
+                    System.out.print(" " + array[m][t] + " ");
+                }
+            }
+            System.out.println(" ");
+        }
+        System.out.println(" ");
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        init();
+        do {
+            switch (turn) {
+            case 0:
+                try {
+                    showTable(field);
+                    System.out.println("白の番です");
+                    System.out.println("x座標を入力");
+                    int x = stdIn.nextInt();
+                    System.out.println("y座標を入力");
+                    int y = stdIn.nextInt();
+                    field[y][x] = 10;
+                    if (turn == 0) {
+                        turn = 1;
+                    } else {
+                        turn = 0;
+                    }
+                    break;
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("正しく座標を入力してください");
+                    break;
+                }
+            case 1:
+                try {
+                    showTable(field);
+                    System.out.println("黒の番です");
+                    System.out.println("x座標を入力");
+                    int a = stdIn.nextInt();
+                    System.out.println("y座標を入力");
+                    int b = stdIn.nextInt();
+                    field[b][a] = 9;
+                    if (turn == 0) {
+                        turn = 1;
+                    } else {
+                        turn = 0;
+                    }
+                    break;
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("正しく座標を入力してください");
+                    break;
+                }
+            }
+        } while (true);
+    }
+}
