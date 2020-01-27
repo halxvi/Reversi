ea371dc16338d64c35b891eea94ec09f4025c748
Kakeru Tsukayama <halxvi@gmail.com>
2020-01-27 18:59:07 +0900

    アプリケーション修正

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -8,6 +8,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 @EnableAutoConfiguration
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
