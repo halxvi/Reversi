6e416ce98d15b4179537d283d04ee0f1337fb783
Kakeru Tsukayama <halxvi@gmail.com>
2020-01-27 18:50:59 +0900

    update

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-public class DemoApplication {
+public class Application {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+
+@Controller
+public class Controller {
+  static List<Integer> data = new ArrayList<Integer>();
+
+  @RequestMapping("/")
+  public String index(Model model) {
+    data.add(1);
+    model.addAttribute("data", data);
+    model.addAttribute("msg", 123);
+    return "index";
+  }
+
+  @RequestMapping("/error")
+  public String index() {
+    return "error";
+  }
+}
