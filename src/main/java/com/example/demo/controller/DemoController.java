829eadfe336cd62662b7273328744d12be190b69
Kakeru Tsukayama <halxvi@gmail.com>
2020-01-27 15:44:07 +0900

    test

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -2,14 +2,13 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.boot.SpringApplication;
+import org.springframework.ui.Model;
 
 @Controller
 public class DemoController {
   @RequestMapping("/")
-  public String index() {
-    // model.addAttribute("msg", 111);
+  public String index(Model model) {
+    model.addAttribute("msg", 111);
     return "index";
   }
 }
