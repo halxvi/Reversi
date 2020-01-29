5ddfc8e42e7e732cab06bac62daa69264f2a0770
Kakeru Tsukayama <halxvi@gmail.com>
2020-01-30 05:01:32 +0900

    frx

--- src/main/resources/static/assets/index.js
+++ src/main/resources/static/assets/index.js
@@ -1,17 +1,16 @@
 function fieldClick(value) {
-    console.log(2)
-    // const data = value.split(":");
-    // const x = data[0];
-    // const y = data[1];
-    // $.ajax({
-    //     type: "GET",
-    //     url: "/update",
-    //     data: { xAxis: x, yAxis: y },
-    //     success: function (response) {
-    //         console.log(response);
-    //     }
-    //     , error: function (res) {
-    //         console.log(res);
-    //     }
-    // });
+    const data = value.split(":");
+    const x = data[0];
+    const y = data[1];
+    $.ajax({
+        type: "GET",
+        url: "/update",
+        data: { xAxis: x, yAxis: y },
+        success: function (response) {
+            console.log(response);
+        }
+        , error: function (res) {
+            console.log(res);
+        }
+    });
 };
