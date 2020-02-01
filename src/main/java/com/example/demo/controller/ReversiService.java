0c3cfe4e86ba0bf21d9ca5875a027f94b481f0f1
Kakeru Tsukayama <halxvi@gmail.com>
2020-02-01 19:51:31 +0900

    fix

--- src/main/java/com/example/demo/controller/ReversiService.java
+++ src/main/java/com/example/demo/controller/ReversiService.java
@@ -702,10 +702,15 @@ class ReversiService {
     }
 
     void checkField(final int x, final int y, final int turn[]) {
+        messageList.add("8");
         if (x > 0 && x < 9 && y > 0 && y < 9) {
+            messageList.add("9");
             if (findVoidPath(x, y) && checkPath(x, y)) {
+                messageList.add("10");
                 flip(x, y, turn);
+                messageList.add("11");
                 changeTurn(turn);
+                messageList.add("12");
             }
         } else {
             messageList.add("正しく座標を入力してください");
